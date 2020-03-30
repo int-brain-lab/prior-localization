c300f443dc9f6de85940358c79ad5ed5cdc906b2
Berk Gercek <gercek.berk@gmail.com>
2020-03-30 18:04:03 +0200

    pstrack fits for all brainwide map sessions

--- prior_funcs.py
+++ prior_funcs.py
@@ -2,9 +2,10 @@ from psytrack.hyperOpt import hyperOpt
 import numpy as np
 from oneibl import one
 from export_funs import trialinfo_to_df
+import os
 
 
-def fit_sess_psytrack(session):
+def fit_sess_psytrack(session, maxlength=2.5):
     '''
     Use Nick's Psytrack code to fit a session. Very slow to run.
 
@@ -25,21 +26,21 @@ def fit_sess_psytrack(session):
     W_std : np.ndarray
         5 x N_trials array. Elementwise corresponds to stdev of each element of wMode.
     '''
-    trialdf = trialinfo_to_df(session, maxlen=99)
+    trialdf = trialinfo_to_df(session, maxlen=maxlength)
     choices = trialdf.choice
     # Remap choice values from -1, 1 to 1, 2 (because psytrack demands it)
     newmap = {-1: 1, 1: 2}
     choices.replace(newmap, inplace=True)
     choices = choices.values[1:]
     tmp = trialdf[['contrastLeft', 'contrastRight']]
-    sL = tmp.contrastLeft.apply(np.nan_to_num).values
-    sL = np.vstack((sL[1:], sL[:-1])).T  # Add a history term going back 1 trial
-    sR = tmp.contrastRight.apply(np.nan_to_num).values
-    sR = np.vstack((sR[1:], sR[:-1])).T  # History term is prior value of contr on that side
+    sL = tmp.contrastLeft.apply(np.nan_to_num).values[1:].reshape(-1, 1)
+    # sL = np.vstack((sL[1:], sL[:-1])).T  # Add a history term going back 1 trial
+    sR = tmp.contrastRight.apply(np.nan_to_num).values[1:].reshape(-1, 1)
+    # sR = np.vstack((sR[1:], sR[:-1])).T  # History term is prior value of contr on that side
 
     data = {'inputs': {'sL': sL, 'sR': sR},
             'y': choices, }
-    weights = {'bias': 1, 'sL': 2, 'sR': 2}
+    weights = {'bias': 1, 'sL': 1, 'sR': 1}
     K = np.sum(list(weights.values()))
     hyper_guess = {'sigma': [2**-5] * K,
                    'sigInit': 2**5,
@@ -51,26 +52,38 @@ def fit_sess_psytrack(session):
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    from ibl_pipeline import subject, ephys
     one = one.ONE()
-    session = one.search(subject='CSHL060', date_range=['2020-03-09', '2020-03-09'],
-                         dataset_types=['spikes.clusters'])[0]
-    wMode, std = fit_sess_psytrack(session)
-    trialnumbers = np.arange(1, wMode.shape[1] + 1)
-    trialdf = trialinfo_to_df(session)
-    plt.figure(figsize=(9, 9))
-    plt.plot(trialnumbers, wMode[0], label='Bias', c='b')
-    plt.plot(trialdf.index, 3 * (trialdf.probabilityLeft - 0.5), color='k')
-    plt.fill_between(trialnumbers, wMode[0] - std[0], wMode[0] + std[0],
-                     color='b', alpha=0.5)
-    plt.plot(trialnumbers, wMode[1], label='Weight contrast Left',
-             color='orange')
-    plt.fill_between(trialnumbers, wMode[1] - std[1], wMode[1] + std[1],
-                     color='orange', alpha=0.5)
-    plt.plot(trialnumbers, wMode[3], label='Weight contrast Right',
-             color='purple')
-    plt.fill_between(trialnumbers, wMode[3] - std[3], wMode[3] + std[3],
-                     color='purple', alpha=0.5)
-    plt.xlabel('Trial number', fontsize=18)
-    plt.ylabel('Weight value', fontsize=18)
-    plt.title('ZM_2240 (Frontal cortex) Psytrack fit', fontsize=24)
-    plt.legend(fontsize=18)
+    sessions = subject.Subject * subject.SubjectProject *\
+        ephys.acquisition.Session * ephys.ProbeTrajectory()
+    bwm_sess = sessions & 'subject_project = "ibl_neuropixel_brainwide_01"' & \
+        'task_protocol = "_iblrig_tasks_ephysChoiceWorld6.2.5"'
+    for s in bwm_sess:
+        sess_id = str(s['session_uuid'])
+        nickname = s['subject_nickname']
+        date = str(s['session_start_time'].date())
+        if not os.path.exists(f'./fits/{nickname}'):
+            os.mkdir(f'./fits/{nickname}')
+        plotfile = f'./fits/{nickname}/{date}_psytrackfit.png'
+        wMode, std = fit_sess_psytrack(sess_id)
+        trialnumbers = np.arange(1, wMode.shape[1] + 1)
+        trialdf = trialinfo_to_df(sess_id, maxlen=2.5)
+        plt.figure(figsize=(9, 9))
+        plt.plot(trialnumbers, wMode[0], label='Bias', c='b')
+        plt.plot(range(len(trialdf)), 3 * (trialdf.probabilityLeft - 0.5), color='k')
+        plt.fill_between(trialnumbers, wMode[0] - std[0], wMode[0] + std[0],
+                         color='b', alpha=0.5)
+        plt.plot(trialnumbers, wMode[1], label='Weight contrast Left',
+                 color='orange')
+        plt.fill_between(trialnumbers, wMode[1] - std[1], wMode[1] + std[1],
+                         color='orange', alpha=0.5)
+        plt.plot(trialnumbers, wMode[2], label='Weight contrast Right',
+                 color='purple')
+        plt.fill_between(trialnumbers, wMode[2] - std[2], wMode[2] + std[2],
+                         color='purple', alpha=0.5)
+        plt.xlabel('Trial number', fontsize=18)
+        plt.ylabel('Weight value', fontsize=18)
+        plt.title(f'{nickname} Psytrack fit', fontsize=24)
+        plt.legend(fontsize=18)
+        plt.savefig(plotfile)
+        plt.close()
