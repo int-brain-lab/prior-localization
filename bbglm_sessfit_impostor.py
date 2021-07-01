2af064f7ed9d53816e5303b8a453b28225c00c20
Berk Gercek <gercek.berk@gmail.com>
2021-07-01 09:18:10 +0200

    start on impostor session code w charles' model

--- bbglm_sessfit_impostor.py
+++ bbglm_sessfit_impostor.py
@@ -7,36 +7,73 @@ Berk, May 2020
 from oneibl import one
 import numpy as np
 import pandas as pd
-from brainbox.modeling import glm, glm_linear
+from oneibl import one
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import RidgeCV
+import brainbox.modeling.design_matrix as dm
+import brainbox.modeling.linear as lm
+import brainbox.modeling.utils as mut
 from synth import _generate_pseudo_blocks
 import brainbox.io.one as bbone
-from prior_funcs import fit_sess_psytrack
+from tqdm import tqdm
+from copy import deepcopy
+from brainbox.population.decode import _generate_pseudo_blocks
+import brainbox.io.one as bbone
 from copy import deepcopy
 from tqdm import tqdm
+from models.expSmoothing_prevAction import expSmoothing_prevAction as exp_prevAct
+from models import utils
+from brainbox.task.closed_loop import generate_pseudo_session
 
-offline = True
-one = one.ONE()
+if __name__ != "__main__":
+    offline = True
+else:
+    offline = False
+one = one.ONE(offline=offline)
+subjects, ins, ins_id, sess_ids, _ = utils.get_bwm_ins_alyx(one)
+
+ephys_cache = {}
 
 
 def fit_session(session_id, kernlen, nbases,
-                t_before=1., t_after=0.6, prior_estimate='psytrack', max_len=2., probe_idx=0,
-                method='minimize', alpha=0, contnorm=5., binwidth=0.02, wholetrial_step=False,
-                blocktrain=False, abswheel=False, no_50perc=False, num_pseudosess=100,):
+                t_before=1., t_after=0.6, prior_estimate='charles', max_len=2., probe_idx=0,
+                contnorm=5., binwidth=0.02, abswheel=False, num_pseudosess=100, progress=True,
+                target_regressor='prior', one=one):
     if not abswheel:
         signwheel = True
     else:
         signwheel = False
     trialsdf = bbone.load_trials_df(session_id, maxlen=max_len, t_before=t_before, t_after=t_after,
-                                    wheel_binsize=binwidth, ret_abswheel=abswheel,
-                                    ret_wheel=signwheel)
-    if prior_estimate == 'psytrack':
-        print('Fitting psytrack esimates...')
-        wts, stds = fit_sess_psytrack(session_id, maxlength=max_len, as_df=True)
-        wts['bias'] = wts['bias'] - np.mean(wts['bias'])
-        fitinfo = pd.concat((trialsdf, wts['bias']), axis=1)
-        bias_next = np.roll(fitinfo['bias'], -1)
-        bias_next = pd.Series(bias_next, index=fitinfo['bias'].index)[:-1]
-        fitinfo['bias_next'] = bias_next
+                                    binsize=binwidth, ret_abswheel=abswheel, ret_wheel=signwheel,
+                                    one=one)
+    if prior_estimate == 'charles':
+        print('Fitting behavioral esimates...')
+        mouse_name = one.get_details(session_id)['subject']
+        stimuli_arr, actions_arr, stim_sides_arr, session_uuids = [], [], [], []
+        for i in range(len(sess_ids)):
+            if subjects[i] == mouse_name:  # take only sessions of first mice
+                data = utils.load_session(sess_ids[i])
+                if data['choice'] is not None and data['probabilityLeft'][0] == 0.5:
+                    stim_side, stimuli, actions, pLeft_oracle = utils.format_data(data)
+                    stimuli_arr.append(stimuli)
+                    actions_arr.append(actions)
+                    stim_sides_arr.append(stim_side)
+                    session_uuids.append(sess_ids[i])
+                    
+        # format data
+        stimuli, actions, stim_side = utils.format_input(
+            stimuli_arr, actions_arr, stim_sides_arr)
+        session_uuids = np.array(session_uuids)
+        model = exp_prevAct('./results/inference/', session_uuids,
+                            mouse_name, actions, stimuli, stim_side)
+        model.load_or_train(remove_old=False)
+        # compute signals of interest
+        signals = model.compute_signal(signal=['prior', 'prediction_error', 'score'],
+                                       verbose=False)
+        trialsdf['prior'] = signals['prior'][trialsdf.index]
+        trialsdf['prior_last'] = pd.Series(np.roll(trialsdf['prior'], 1), index=trialsdf.index)
+        fitinfo = trialsdf.copy()
     elif prior_estimate is None:
         fitinfo = trialsdf.copy()
     else:
@@ -54,14 +91,9 @@ def fit_session(session_id, kernlen, nbases,
     spk_times = spikes[probestr].times
     spk_clu = spikes[probestr].clusters
     clu_regions = clusters[probestr].acronym
-    fitinfo['pLeft_last'] = pd.Series(np.roll(fitinfo['probabilityLeft'], 1),
-                                      index=fitinfo.index)[:-1]
     fitinfo = fitinfo.iloc[1:-1]
     fitinfo['adj_contrastLeft'] = np.tanh(contnorm * fitinfo['contrastLeft']) / np.tanh(contnorm)
     fitinfo['adj_contrastRight'] = np.tanh(contnorm * fitinfo['contrastRight']) / np.tanh(contnorm)
-
-    if no_50perc:
-        fitinfo = fitinfo[fitinfo.probabilityLeft != 0.5]
 
     vartypes = {'choice': 'value',
                 'response_times': 'timing',
@@ -83,73 +115,104 @@ def fit_session(session_id, kernlen, nbases,
     if t_before < 0.7:
         raise ValueError('t_before needs to be 0.7 or greater in order to do -0.1 to -0.7 step'
                          ' function on pLeft')
- 
-    def stepfunc(row):
-        currvec = np.ones(nglm.binf(row.duration)) * row.pLeft_last
-        # nextvec = np.ones(nglm.binf(row.duration) - nglm.binf(row.feedback_times)) *\
-        #     row.probabilityLeft
-        return currvec
 
     def stepfunc_prestim(row):
-        stepvec = np.zeros(nglm.binf(row.duration))
-        stepvec[stepbounds[0]:stepbounds[1]] = row.pLeft_last
+        stepvec = np.zeros(design.binf(row.duration))
+        stepvec[stepbounds[0]:stepbounds[1]] = row.prior_last
         return stepvec
 
-    def stepfunc_bias(row):
-        currvec = np.ones(nglm.binf(row.feedback_times)) * row.bias
-        nextvec = np.ones(nglm.binf(row.duration) - nglm.binf(row.feedback_times)) *\
-            row.bias_next
-        return np.hstack((currvec, nextvec))
+    def stepfunc_poststim(row):
+        vec = np.zeros(design.binf(row.duration))
+        currtr_start = design.binf(row.stimOn_times + 0.1)
+        currtr_end = design.binf(row.feedback_times)
+        vec[currtr_start:currtr_end] = row.prior_last
+        vec[currtr_end:] = row.prior
+        return vec
 
-    nglm = glm_linear.LinearGLM(fitinfo, spk_times, spk_clu, vartypes, binwidth=binwidth,
-                                blocktrain=blocktrain)
-    nglm.clu_regions = clu_regions
-    stepbounds = [nglm.binf(0.1), nglm.binf(0.6)]
 
-    cosbases_long = glm.full_rcos(kernlen, nbases, nglm.binf)
-    cosbases_short = glm.full_rcos(0.4, nbases, nglm.binf)
-    nglm.add_covariate_timing('stimonL', 'stimOn_times', cosbases_long,
-                              cond=lambda tr: np.isfinite(tr.contrastLeft),
-                              deltaval='adj_contrastLeft',
-                              desc='Kernel conditioned on L stimulus onset')
-    nglm.add_covariate_timing('stimonR', 'stimOn_times', cosbases_long,
-                              cond=lambda tr: np.isfinite(tr.contrastRight),
-                              deltaval='adj_contrastRight',
-                              desc='Kernel conditioned on R stimulus onset')
-    nglm.add_covariate_timing('correct', 'feedback_times', cosbases_long,
-                              cond=lambda tr: tr.feedbackType == 1,
-                              desc='Kernel conditioned on correct feedback')
-    nglm.add_covariate_timing('incorrect', 'feedback_times', cosbases_long,
-                              cond=lambda tr: tr.feedbackType == -1,
-                              desc='Kernel conditioned on incorrect feedback')
-    if prior_estimate is None and wholetrial_step:
-        nglm.add_covariate_raw('pLeft', stepfunc, desc='Step function on prior estimate')
-    elif prior_estimate is None and not wholetrial_step:
-        nglm.add_covariate_raw('pLeft', stepfunc_prestim,
-                               desc='Step function on prior estimate')
-    elif prior_estimate == 'psytrack':
-        nglm.add_covariate_raw('pLeft', stepfunc_bias, desc='Step function on prior estimate')
-    nglm.add_covariate('wheel', fitinfo['wheel_velocity'], cosbases_short, -0.4)
-    nglm.compile_design_matrix()
+    design = dm.DesignMatrix(trdf, vartypes, binwidth=binwidth)
+    stepbounds = [design.binf(t_before - 0.6), design.binf(t_before - 0.1)]
+
+    cosbases_long = mut.full_rcos(kernlen, nbases, design.binf)
+    cosbases_short = mut.full_rcos(0.4, nbases, design.binf)
+    design.add_covariate_timing('stimonL', 'stimOn_times', cosbases_long,
+                                cond=lambda tr: np.isfinite(tr.contrastLeft),
+                                deltaval='adj_contrastLeft',
+                                desc='Kernel conditioned on L stimulus onset')
+    design.add_covariate_timing('stimonR', 'stimOn_times', cosbases_long,
+                                cond=lambda tr: np.isfinite(tr.contrastRight),
+                                deltaval='adj_contrastRight',
+                                desc='Kernel conditioned on R stimulus onset')
+    design.add_covariate_timing('correct', 'feedback_times', cosbases_long,
+                                cond=lambda tr: tr.feedbackType == 1,
+                                desc='Kernel conditioned on correct feedback')
+    design.add_covariate_timing('incorrect', 'feedback_times', cosbases_long,
+                                cond=lambda tr: tr.feedbackType == -1,
+                                desc='Kernel conditioned on incorrect feedback')
+    design.add_covariate_raw('prior', stepfunc_prestim,
+                             desc='Step function on prior estimate')
+    design.add_covariate_raw('prior_tr', stepfunc_poststim,
+                             desc='Step function on post-stimulus prior')
+    design.add_covariate('wheel', trdf['wheel_velocity'], cosbases_short, -0.4)
+    design.compile_design_matrix()
+
+    _, s, v = np.linalg.svd(design[:, design.covar['wheel']['dmcol_idx']], full_matrices=False)
+    variances = s**2 / (s**2).sum()
+    n_keep = np.argwhere(np.cumsum(variances) >= 0.9999)[0, 0]
+    wheelcols = design[:, design.covar['wheel']['dmcol_idx']]
+    reduced = wheelcols @ v[:n_keep].T
+    bases_reduced = cosbases_short @ v[:n_keep].T
+    keepcols = ~np.isin(np.arange(design.dm.shape[1]), design.covar['wheel']['dmcol_idx'])
+    basedm = design[:, keepcols]
+    design.dm = np.hstack([basedm, reduced])
+    design.covar['wheel']['dmcol_idx'] = design.covar['wheel']['dmcol_idx'][:n_keep]
+    design.covar['wheel']['bases'] = bases_reduced
+
+    print(np.linalg.cond(design.dm))
+    trialinds = np.array([(tr, np.searchsorted(design.trlabels.flat, tr))
+                          for tr in design.trialsdf.index])
+    tmparr = np.roll(trialinds[:, 1], -1)
+    tmparr[-1] = design.dm.shape[0]
+    trialinds = np.hstack((trialinds, tmparr.reshape(-1, 1)))
+
+    nglm = lm.LinearGLM(design, spk_times, spk_clu, estimator=RidgeCV(cv=3))
     glm_template = deepcopy(nglm)
-    nglm.fit(method=method, printcond=False)
+    nglm.fit(printcond=False)
     realscores = nglm.score()
+    nglm.clu_regions = clu_regions
 
     scoreslist = []
     weightslist = []
-    for i in tqdm(range(num_pseudosess), desc='Pseudo block iteration num', leave=False):
-        newblocks = _generate_pseudo_blocks(len(fitinfo))
+    for _ in tqdm(range(num_pseudosess), desc='Pseudo block iteration num', leave=False,
+                  disable=not progress):
+        newsess = generate_pseudo_session(trdf)
+        signals = model.compute_signal(signal=['prior', 'prediction_error'])
         probmap = {1: 0.8, 0: 0.2}
         newprobs = [probmap[i] for i in newblocks]
-        tmp_df = fitinfo.copy()
+        tmp_df = design.trialsdf.copy()
         tmp_df['probabilityLeft'] = newprobs
+        tmp_df['pLeft_last'] = pd.Series(np.roll(tmp_df['probabilityLeft'], 1),
+                                         index=tmp_df.index)
         tmpglm = deepcopy(glm_template)
-
-        tmpglm.fit(method=method, printcond=False)
+        pl_idx = design.covar['pLeft']['dmcol_idx']
+        plt_idx = design.covar['pLeft_tr']['dmcol_idx']
+        for tr, start, end in trialinds:
+            if target_regressor == 'pLeft':
+                tmpglm.design.dm[start:end, pl_idx][tmpglm.design.dm[start:end, pl_idx]
+                                                    > 0] = tmp_df.pLeft_last[tr]
+            else:
+                pl_old = trdf.probabilityLeft[tr]
+                pll_old = trdf.pLeft_last[tr]
+                tmpglm.design.dm[start:end, plt_idx][tmpglm.design.dm[start:end,
+                                                     plt_idx] == pl_old] = tmp_df.probabilityLeft[tr]
+                tmpglm.design.dm[start:end, plt_idx][tmpglm.design.dm[start:end,
+                                                     plt_idx] == pll_old] = tmp_df.pLeft_last[tr]
+        tmpglm.fit(printcond=False)
         weightslist.append((tmpglm.coefs, tmpglm.intercepts))
         with np.errstate(all='ignore'):
             scoreslist.append(tmpglm.score())
     return nglm, realscores, scoreslist, weightslist
+
 
 
 if __name__ == "__main__":
