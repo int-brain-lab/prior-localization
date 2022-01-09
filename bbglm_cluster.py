697d135481129831b05bff99f81cdebfc2beb3e5
Berk Gercek <gercek.berk@gmail.com>
2022-01-09 19:44:55 +0100

    Pseudosess on hold, stepwise in progress

--- bbglm_cluster.py
+++ bbglm_cluster.py
@@ -9,11 +9,9 @@ import os
 from datetime import date
 from glob import glob
 from pathlib import Path
-from sklearn.base import RegressorMixin
-from sklearn.model_selection import KFold, GridSearchCV
 from one.api import ONE
 from brainbox.task.closed_loop import generate_pseudo_session
-from bbglm_sessfit import load_regressors, generate_design
+from bbglm_sessfit import fit, fit_stepwise, generate_design
 from .decoding.decoding_utils import compute_target, query_sessions
 from params import BEH_MOD_PATH, GLM_FIT_PATH
 
@@ -24,40 +22,10 @@ def get_cached_regressors(fpath):
     return data['trialsdf'], data['spk_times'], data['spk_clu'], data['clu_reg'], data['clu_qc']
 
 
-def fit(design, spk_t, spk_clu, binwidth, model, estimator, n_folds=5, contiguous=False):
-    trials_idx = design.trialsdf.index
-    nglm = model(design, spk_t, spk_clu, binwidth=binwidth, estimator=estimator)
-    splitter = KFold(n_folds, shuffle=~contiguous)
-    scores, weights, intercepts, alphas, splits = [], [], [], [], []
-    for test, train in splitter.split(trials_idx):
-        nglm.fit(train_idx=train, printcond=False)
-        if isinstance(estimator, GridSearchCV):
-            alphas.append(estimator.best_params_['alpha'])
-        elif isinstance(estimator, RegressorMixin):
-            alphas.append(estimator.get_params()['alpha'])
-        else:
-            raise TypeError('Estimator must be a sklearn linear regression instance')
-        intercepts.append(nglm.intercepts)
-        weights.append(nglm.combine_weights())
-        scores.append(nglm.score(testinds=test))
-        splits.append({'test': test, 'train': train})
-    outdict = {
-        'scores': scores,
-        'weights': weights,
-        'intercepts': intercepts,
-        'alphas': alphas,
-        'splits': splits
-    }
-    return outdict
-
-
-def save(subject, session_id, fitout, params):
-    subpath = Path(GLM_FIT_PATH).joinpath(subject)
-    if not subpath.exists():
-        os.mkdir(subpath)
-    sesspath = subpath.joinpath(session_id)
-    if not sesspath.exists():
-        os.mkdir(sesspath)
+def save_pseudo(subject, session_id, fitout, params):
+    # TODO: Make this work
+    raise NotImplementedError
+    sesspath = _create_sub_sess_path(GLM_FIT_PATH, subject, session_id)
     fn = sesspath.joinpath()
     subjfilepath = os.path.abspath(filename)
     fw = open(subjfilepath, 'wb')
@@ -66,14 +34,30 @@ def save(subject, session_id, fitout, params):
     return True
 
 
-def check_fit_exists(filename):
-    if not os.path.exists(savepath + nickname):
-        os.mkdir(savepath + nickname)
-    subpaths = [n for n in glob(os.path.abspath(filename)) if os.path.isfile(n)]
-    if len(subpaths) != 0:
-        return True
-    else:
-        return False
+def save_stepwise(subject, session_id, fitout, params, probes, input_fn, clu_reg, clu_qc, fitdate):
+    sesspath = _create_sub_sess_path(GLM_FIT_PATH, subject, session_id)
+    fn = sesspath.joinpath(f'{fitdate}_stepwise_regression.pkl')
+    outdict = {
+        'params': params,
+        'probes': probes,
+        'model_input_fn': input_fn,
+        'clu_regions': clu_reg,
+        'clu_qc': clu_qc,
+    }
+    outdict.update(fitout)
+    with open(fn, 'wb') as fw:
+        pickle.dump(outdict)
+    return fn
+
+
+def _create_sub_sess_path(parent, subject, session):
+    subpath = Path(parent).joinpath(subject)
+    if not subpath.exists():
+        os.mkdir(subpath)
+    sesspath = subpath.joinpath(session)
+    if not sesspath.exists():
+        os.mkdir(sesspath)
+    return sesspath
 
 
 if __name__ == "__main__":
@@ -104,7 +88,9 @@ if __name__ == "__main__":
         'reduce_wheel_dim': True,
         'dataset_fn': '2022-01-03_dataset_metadata.pkl',
         'model': lm.LinearGLM,
-        'estimator': skl.Ridge
+        'estimator': skl.Ridge,
+        'contiguous': False,
+        # 'n_pseudo': 100,
     }
 
     currdate = str(date.today())
@@ -122,12 +108,30 @@ if __name__ == "__main__":
     dload = dask.delayed(get_cached_regressors, nout=5)
     dprior = dask.delayed(compute_target)
     ddesign = dask.delayed(generate_design)
-    dfit = dask.delayed(fit)
-    dsave = dask.delayed(save)
+    dpseudo = dask.delayed(generate_pseudo_session)
+    dfit = dask.delayed(fit_stepwise)
+    dsave = dask.delayed(save_stepwise)
     
-    for i, (subject, eid, probe) in sessions.iterrows():
+    data_fns = []
+    for i, (subject, eid, probes, eidfn) in dataset_fns.iterrows():
         subjeids = sessions.xs(subject, level='subject').eid.to_list()
-        probes, eidfn = dataset_fns.loc[subject, eid]
         stdf, sspkt, sspkclu, sclureg, scluqc = dload(eidfn)
         sessprior = dprior('prior', subjeids, subjeids, BEH_MOD_PATH)
         sessdesign = ddesign(stdf, sessprior, dataset_params['t_before'], **params)
+        sessfit = dfit(sessdesign, sspkt, sspkclu, **params)
+        outputfn = dsave(subject, eid, sessfit, params, probes, eidfn, sclureg, scluqc, currdate)
+        data_fns.append(outputfn)
+
+    N_CORES = 4
+    cluster = SLURMCluster(cores=N_CORES, memory='32GB', processes=1, queue="shared-cpu",
+                           walltime="01:15:00",
+                           log_directory='/home/gercek/dask-worker-logs',
+                           interface='ib0',
+                           extra=["--lifetime", "60m", "--lifetime-stagger", "10m"],
+                           job_cpu=N_CORES, env_extra=[f'export OMP_NUM_THREADS={N_CORES}',
+                                                       f'export MKL_NUM_THREADS={N_CORES}',
+                                                       f'export OPENBLAS_NUM_THREADS={N_CORES}'])
+    cluster.adapt(minimum_jobs=0, maximum_jobs=400)
+    client = Client(cluster)
+    futures = client.compute(data_fns)
+    
