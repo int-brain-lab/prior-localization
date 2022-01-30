e392152224b429a1b5ed3c5930f483c82f82e354
csmfindling <charles.findling@gmail.com>
2022-01-30 21:16:43 +0100

    LOCAL=FALSE

--- decoding/decode_prior.py
+++ decoding/decode_prior.py
@@ -42,7 +42,7 @@ strlut = {sklm.Lasso: "Lasso",
           sklm.LogisticRegression: "Logistic"}
 
 # %% Run param definitions
-LOCAL = True
+LOCAL = False
 if LOCAL:
     DECODING_PATH = Path("/Users/csmfindling/Documents/Postdoc-Geneva/IBL/behavior/prior-localization/decoding")
 else:
