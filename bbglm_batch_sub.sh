19ee8cf356fbfe7ae20d02bcbfb3ea966fc2fb67
Berk Gercek <gercek.berk@gmail.com>
2022-02-06 15:17:10 +0100

    Added processing code for GLM results

--- bbglm_batch_sub.sh
+++ bbglm_batch_sub.sh
@@ -10,7 +10,7 @@
 # run one task which use all the cpus of the node
 conda activate iblenv
 python /home/gercek/Projects/prior-localization/bbglm_cluster_worker.py \
- /home/gercek/scratch/glm_cache/2022-01-19_dataset_metadata.pkl \
- /home/gercek/scratch/results/glms/2022-02-03_glm_fit_pars.pkl \
- ${SLURM_ARRAY_TASK_ID} \
- 2022-02-03
+ /home/gercek/scratch/glm_cache/2022-01-19_dataset_metadata.pkl \ # Dataset file name
+ /home/gercek/scratch/results/glms/2022-02-03_glm_fit_pars.pkl \ # GLM parameters file name
+ ${SLURM_ARRAY_TASK_ID} \ # Worker index into dataset['dataset_filenames']
+ 2022-02-03 # Fitting date for output file
