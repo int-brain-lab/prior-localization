92b6edd306205dc911d6c01a7c6c4e50a5d26e2c
Berk Gercek <gercek.berk@gmail.com>
2022-03-08 11:30:14 +0100

    Revert to pandas instead of xarray. Too much pain, not enough payoff

--- brainwide/encoding/pipelines/03b_gather_results_impostor.py
+++ brainwide/encoding/pipelines/03b_gather_results_impostor.py
@@ -5,7 +5,6 @@ from functools import cache
 # Third party libraries
 import numpy as np
 import pandas as pd
-import xarray as xr
 from tqdm import tqdm
 
 # IBL libraries
@@ -19,87 +18,57 @@ def generate_da_dict(
         filename,
         n_impostors,
         n_folds,
-        region_mapper=lambda x: x,
-        temporal_kernels=['wheel', 'stimonL', 'stimonR', 'fmove', 'correct', 'incorrect'],
-        gain_kernels=['pLeft', 'pLeft_tr']):
+        region_mapper=lambda x: x,):
     datafile = pd.read_pickle(filename)
     eid = filename.parts[-2]  # Ugly hack because I forgot to keep eid value in files
-    darrays = process_temporal_kernels(datafile, eid, temporal_kernels, n_impostors, n_folds,
-                                       region_mapper)
-
-    darrays_gain = process_gain_kernels(datafile, eid, gain_kernels, n_impostors, n_folds,
-                                        region_mapper)
-    null_dist = {**darrays, **darrays_gain}
-
-    return null_dist
+    dfs = process_kernels(datafile, eid, n_impostors, n_folds, region_mapper)
+    return dfs
 
 
-def process_gain_kernels(datafile, eid, kern_names, n_impostors, n_folds, reg_map):
-    darrays = defaultdict(list)
-    for kernel in kern_names:
+def process_kernels(datafile, eid, n_impostors, n_folds, reg_map, get_kweights=False):
+    dfs = defaultdict(list)
+    for kernel in datafile['fitdata']['weights'][0].keys():
         for i in range(-1, n_impostors):
-            fold_da = []
             for j in range(n_folds):
                 if i < 0:  # this references the actual data fit
                     df = datafile['fitdata']['weights'][j][kernel].copy()
+                    ref_idx = df.index.copy()
                 else:
-                    df = datafile['nullfits'][i]['weights'][j][kernel].copy()
-                mi = make_multiindex(datafile, eid, reg_map, df)
+                    df = datafile['nullfits'][i]['weights'][j][kernel].copy().reindex(ref_idx)
+                mi = make_multiindex(datafile, eid, reg_map, df, j, i)
                 df.index = mi
-                tmp_da = xr.DataArray(
-                    df.values,
-                    dims=['unit'],
-                    coords={'unit': df.index},
-                )
-                fold_da.append(tmp_da)
-            tmp_da = xr.concat(fold_da, pd.Index(np.arange(n_folds), name='fold'))
-            darrays[kernel].append(tmp_da)
-    darrays_cat = {
-        k: xr.concat(darrays[k], pd.Index(np.arange(-1, n_impostors), name='run')) for k in darrays
-    }
-    return darrays_cat
+                dfs[kernel].append(df)
+    dfs_cat = {k: pd.concat(dfs[k]).sort_index() for k in dfs}
+    return dfs_cat
 
 
-def process_temporal_kernels(datafile, eid, kern_names, n_impostors, n_folds, reg_map):
-    darrays = defaultdict(list)
-    for kernel in kern_names:
-        for i in range(-1, n_impostors):
-            fold_da = []
-            for j in range(n_folds):
-                if i < 0:  # this references the actual data fit
-                    df = datafile['fitdata']['weights'][j][kernel].copy()
-                else:
-                    df = datafile['nullfits'][i]['weights'][j][kernel].copy()
-                mi = make_multiindex(datafile, eid, reg_map, df)
-                df.index = mi
-                tmp_da = xr.DataArray(
-                    df.values,
-                    dims=['unit', 'time'],
-                    coords={
-                        'unit': df.index,
-                        'time': df.columns
-                    },
-                )
-                fold_da.append(tmp_da)
-            tmp_da = xr.concat(fold_da, pd.Index(np.arange(n_folds), name='fold'))
-            darrays[kernel].append(tmp_da)
-    darrays_cat = {
-        k: xr.concat(darrays[k], pd.Index(np.arange(-1, n_impostors), name='run')) for k in darrays
-    }
-    return darrays_cat
-
-
-def make_multiindex(datafile, eid, reg_map, df):
+def make_multiindex(datafile, eid, reg_map, df, fold, impostor):
     mi = pd.MultiIndex.from_arrays(
         [
             np.array([eid] * len(df)),
             df.index,
             np.array([reg_map(id) for id in datafile['clu_regions'][df.index]]).flatten(),
+            np.ones_like(df.index) * impostor,
+            np.ones_like(df.index) * fold,
         ],
-        names=['eid', 'clu_id', 'region'],
+        names=['eid', 'clu_id', 'region', 'nullrun', 'fold'],
     )
-
     return mi
+
+
+def extract_kweights(df: pd.DataFrame, bases=None):
+    def lstsq(row):
+        try:
+            return np.linalg.lstsq(bases, row)[0]
+        except np.linalg.LinAlgError:
+            return np.nan * np.ones(bases.shape[1])
+
+    if len(df.columns) == 1:
+        return df
+    else:
+        newdf = df.apply(lstsq, axis=1, result_type='expand')
+        newdf.columns = np.arange(bases.shape[1])
+        return newdf
 
 
 if __name__ == '__main__':
@@ -120,11 +89,20 @@ if __name__ == '__main__':
     with open(datapath, 'rb') as fo:
         dataset = pickle.load(fo)
 
+    # Make a directory to store results from this fit run
+    fitfolder = Path(GLM_FIT_PATH).joinpath('merged_results').joinpath(f'{FITDATE}_impostor_run')
+    try:
+        os.mkdir(fitfolder)
+    except FileExistsError:
+        pass
+
     n_folds = params['n_folds'] if 'n_folds' in params else 5
     n_impostors = params['n_impostors']
 
     filenames = []
     for subj in os.listdir(Path(GLM_FIT_PATH)):
+        if subj == 'merged_results':
+            continue
         subjdir = Path(GLM_FIT_PATH).joinpath(subj)
         if not os.path.isdir(subjdir):
             continue
@@ -144,11 +122,20 @@ if __name__ == '__main__':
 
     fdata = defaultdict(list)
     for filename in tqdm(filenames):
-        darrays = generate_da_dict(filename, n_impostors, n_folds, regmap)
-        for k in darrays:
-            fdata[k].append(darrays[k])
-    fdata['params'] = params
-    fdata['dataset'] = dataset
+        dfs = generate_da_dict(filename, n_impostors, n_folds, regmap)
+        for k in dfs:
+            fdata[k].append(dfs[k])
 
-    with open(Path(GLM_FIT_PATH).joinpath(f'{FITDATE}_impostor_fit.pkl'), 'wb') as fw:
-        pickle.dump(fdata, fw)
+    for k in list(fdata.keys()):
+        fdata[k] = pd.concat(fdata[k])
+        if isinstance(fdata[k], pd.DataFrame):
+            fdata[k].columns = fdata[k].columns.astype(str)
+        else:
+            fdata[k] = fdata[k].to_frame()
+        fdata[k].to_parquet(fitfolder.joinpath(f'{k}_data.parquet'))
+        del fdata[k]
+
+    with open(fitfolder.joinpath('fit_params.pkl'), 'wb') as fw:
+        pickle.dump(params, fw)
+    with open(fitfolder.joinpath('fit_dataset.pkl'), 'wb') as fw:
+        pickle.dump(dataset, fw)
