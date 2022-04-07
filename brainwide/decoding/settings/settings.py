632cabe34429470d73dccfee933b3b7c2d4dd4ff
Hubert Félix <hubertf@login2.baobab>
2022-04-07 10:28:51 +0200

    current settings

--- brainwide/decoding/settings/settings.py
+++ brainwide/decoding/settings/settings.py
@@ -26,7 +26,7 @@ else:
 # aligned -> histology was performed by one experimenter
 # resolved -> histology was performed by 2-3 experiments
 SESS_CRITERION = 'aligned-behavior'  # aligned and behavior
-DATE = '2022-04-05'
+DATE = '2022-04-08'
 ALIGN_TIME = 'goCue_times'
 TARGET = 'pLeft'  # 'signcont' or 'pLeft'
 CONTINUOUS_TARGET = False  # True  # is target continuous or not
@@ -58,13 +58,13 @@ if NORMALIZE_INPUT or NORMALIZE_OUTPUT:
     warnings.warn('This feature has not been tested')
 USE_IMPOSTER_SESSION = True  # if false, it uses pseudosessions
 
-BALANCED_WEIGHT = True  # seems to work better with BALANCED_WEIGHT=False, but putting True is important
+BALANCED_WEIGHT = True # until implemented the importance sampling   # seems to work better with BALANCED_WEIGHT=False, but putting True is important
 USE_OPENTURNS = False  # uses openturns to perform kernel density estimation
 BIN_SIZE_KDE = 0.05  # size of the kde bin
 HPARAM_GRID = {'alpha': np.array([0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10])}
 SAVE_BINNED = False  # Debugging parameter, not usually necessary
 COMPUTE_NEURO_ON_EACH_FOLD = False  # if True, expect a script that is 5 times slower
-ADD_TO_SAVING_PATH = 'imposterSessions_mergedProbes_wholeBrain_2'
+ADD_TO_SAVING_PATH = 'imposterSessions_mergedProbes_wholeBrain'
 
 # WIDE FIELD IMAGING
 WIDE_FIELD_IMAGING = False
