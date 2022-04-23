6e0f9b9d17f904c0f56d492145b89068a7caadd1
csmfindling <charles.findling@gmail.com>
2022-04-23 23:12:50 +0200

    update

--- brainwide/decoding/settings/settings.py
+++ brainwide/decoding/settings/settings.py
@@ -26,7 +26,7 @@ else:
 # aligned -> histology was performed by one experimenter
 # resolved -> histology was performed by 2-3 experiments
 SESS_CRITERION = 'aligned-behavior'  # aligned and behavior
-DATE = '2022-04-23'  # str(date.today())  # '2022-04-18'
+DATE = '2022-04-24'  # str(date.today())  # '2022-04-18'
 ALIGN_TIME = 'goCue_times'
 TARGET = 'pLeft'  # 'signcont' or 'pLeft'
 if TARGET not in ['pLeft', 'signcont', 'choice', 'feedback']:
@@ -63,7 +63,7 @@ if NORMALIZE_INPUT or NORMALIZE_OUTPUT:
 USE_IMPOSTER_SESSION = False  # if false, it uses pseudosessions
 USE_IMPOSTER_SESSION_FOR_BALANCING = False  # if false, it simulates the model (should be False)
 
-BALANCED_WEIGHT = False  # seems to work better with BALANCED_WEIGHT=False, but putting True is important
+BALANCED_WEIGHT = True  # seems to work better with BALANCED_WEIGHT=False, but putting True is important
 USE_OPENTURNS = False  # uses openturns to perform kernel density estimation
 BIN_SIZE_KDE = 0.05  # size of the kde bin
 HPARAM_GRID = ({'alpha': np.array([0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10])} if not (sklm.LogisticRegression == ESTIMATOR)
