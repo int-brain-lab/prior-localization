3b5497bda8f78cd012e6490723930fc6dfbc6866
csmfindling <charles.findling@gmail.com>
2022-06-08 21:59:23 +0200

    ipdate

--- braindelphi/decoding/slurm.sh
+++ braindelphi/decoding/slurm.sh
@@ -7,7 +7,9 @@
 #SBATCH --mem=7000
 #SBATCH --time=12:00:00
 
-conda activate iblenv
+source /home/users/f/findling/.bash_profile
+mamba activate iblenv
+
 # extracting settings from $SLURM_ARRAY_TASK_ID
 echo index $SLURM_ARRAY_TASK_ID
 
