7e27155b642858660ac364e6ec897b0a285bf0d8
juhuntenburg <ju.huntenburg@gmail.com>
2022-09-20 18:55:23 +0200

    start new cache and loaders

---
+++ braindelphi/pipelines/download_data.py
@@ -0,0 +1,19 @@
+from one.api import ONE
+from brainbox.io.one import SessionLoader
+from brainwidemap import bwm_query, load_good_units
+
+
+one = ONE()
+bwm_df = bwm_query(one)
+bwm_df = bwm_df.iloc[:6]
+
+# Download trials data
+for eid in bwm_df['eid'].unique():
+    print(f'Downloading trials for {eid}')
+    sess_loader = SessionLoader(one, eid)
+    sess_loader.load_trials()
+
+# Download ephys data
+for pid in bwm_df['pid']:
+    print(f'Downloading spike sorting for {pid}')
+    spikes, clusters = load_good_units(one, pid)
