27a9bb99f760f5f3b312315eb3ec7a96c283770d
csmfindling <charles.findling@gmail.com>
2023-02-12 14:19:49 +0100

    updating ephys cache

--- code/pipelines/01_cache_ephys.py
+++ code/pipelines/01_cache_ephys.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 from pathlib import Path
 from code.pipelines.utils_pipelines import load_ephys
 from code.pipelines.utils_pipelines import cache_regressors
-
+import numpy as np
 
 # Third party libraries
 import pandas as pd
@@ -27,7 +27,6 @@ ALGN_RESOLVED = True
 DATE = str(dt.today())
 QC = True
 TYPE = "primaries"
-MERGE_PROBES = False
 # End parameters
 
 # Construct params dict from above
@@ -50,26 +49,16 @@ for eid in tqdm(bwm_df.index.unique(level="eid")):
     # load and save data from each probe independently, or merge the data from both probes
     pids = session_df.pid.to_list()
     probe_names = session_df.probe_name.to_list()
-    if MERGE_PROBES:
-        raise NotImplementedError("There is problem here")
-        load_outputs = delayed_load(eid, pids, params)
-        save_future = delayed_save(
-            subject, eid, "merged_probes", {**params, "type": TYPE}, load_outputs
-        )
-        # dataset_futures.append([subject, eid, "merged_probes", save_future])
-    else:
-        for (pid, probe_name) in zip(pids, probe_names):
-            try:
-                outputs = load_ephys(eid, [pid], one=one, **params)
-                meta_file, reg_file = cache_regressors(
-                    subject, eid, probe_name, {**params, "type": TYPE}, outputs
-                )
-                dataset_.append([subject, eid, probe_name, meta_file, reg_file])
-            except:
-                fails.append((pid, probe_name))
-                pass
-
-import numpy as np
+    for (pid, probe_name) in zip(pids, probe_names):
+        try:
+            outputs = load_ephys(eid, [pid], one=one, **params)
+            meta_file, reg_file = cache_regressors(
+                subject, eid, probe_name, {**params, "type": TYPE}, outputs
+            )
+            dataset_.append([subject, eid, probe_name, meta_file, reg_file])
+        except:
+            fails.append((pid, probe_name))
+            pass
 
 for eid, eid_probe_name in np.array(fails):
     if eid not in bwm_df.index.get_level_values(1):
@@ -77,6 +66,7 @@ for eid, eid_probe_name in np.array(fails):
         continue
     session_df = bwm_df.xs(eid, level="eid")
     subject = session_df.index[0]
+
     # If there are two probes, there are two options:
     # load and save data from each probe independently, or merge the data from both probes
     pids = session_df.pid.to_list()
@@ -87,13 +77,6 @@ for eid, eid_probe_name in np.array(fails):
                 subject, eid, probe_name, {**params, "type": TYPE}, outputs
             )
             dataset_.append([subject, eid, probe_name, meta_file, reg_file])
-"""
-fails
-[('04690e35-ab38-41db-982c-50cbdf8d0dd1', 'probe01'),
- ('e10a7a75-4740-41d1-82bb-7696ed14c442', 'probe01'),
- ('100433fa-2c59-4432-8295-aa27657fe3fb', 'probe00'),
- ('d591a59c-b49b-46ba-a914-df379ada9813', 'probe01')]
-"""
 
 dataset = pd.DataFrame(
     dataset_, columns=["subject", "eid", "probe_name", "meta_file", "reg_file"]
@@ -102,12 +85,3 @@ dataset = pd.DataFrame(
 outdict = {"params": params, "dataset_filenames": dataset}
 with open(Path(CACHE_PATH).joinpath(DATE + "_ephys_metadata.pkl"), "wb") as fw:
     pickle.dump(outdict, fw)
-
-"""
-with open(
-    Path(CACHE_PATH).joinpath("2023-01-18 08:50:47.774747_ephys_metadata.pkl"), "rb"
-) as fw:
-    outdict = pickle.load(fw)
-dataset = outdict['dataset_filenames']
-dataset_ = dataset.values.tolist()
-"""
