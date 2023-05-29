a283d9f7defa8215eb230fe756cff256573fb4e0
csmfindling <charles.findling@gmail.com>
2023-05-29 15:27:10 +0200

    removed commented code

--- prior_pipelines/decoding/functions/neurometric.py
+++ prior_pipelines/decoding/functions/neurometric.py
@@ -28,37 +28,6 @@ def compute_neurometric_prior(trialsdf_neurometric, metadata, **kwargs):
         trialsdf_neurometric["blockprob_neurometric"] = blockprob_neurometric
     return trialsdf_neurometric
 
-
-
-"""
-trialsdf_neurometric = nb_trialsdf.reset_index() if (pseudo_id == -1) else \
-    pseudosess[pseudomask].reset_index()
-if kwargs['model'] is not None:
-    blockprob_neurometric = dut.compute_target(
-        'pLeft',
-        subject,
-        subjeids,
-        eid,
-        kwargs['modelfit_path'],
-        binarization_value=kwargs['binarization_value'],
-        modeltype=kwargs['model'],
-        beh_data_test=trialsdf if pseudo_id == -1 else pseudosess,
-        behavior_data_train=behavior_data_train,)
-
-    trialsdf_neurometric['blockprob_neurometric'] = np.stack([
-        np.greater_equal(
-            blockprob_neurometric[(mask & (trialsdf.choice != 0) if pseudo_id
-                                                                    == -1 else pseudomask)], border).astype(int)
-        for border in kwargs['border_quantiles_neurometric']
-    ]).sum(0)
-
-else:
-    blockprob_neurometric = trialsdf_neurometric['probabilityLeft'].replace(
-        0.2, 0).replace(0.8, 1)
-    trialsdf_neurometric['blockprob_neurometric'] = blockprob_neurometric
-"""
-
-
 def get_target_df(target, pred, test_idxs, trialsdf):
     """
     Get two arrays needed for the psychofit method for fitting an erf.
