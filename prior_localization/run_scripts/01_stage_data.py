a21e4718d598ec6f74c4361c9a90a28fd485915d
juhuntenburg <ju.huntenburg@gmail.com>
2023-09-20 15:31:12 +0200

    first run scripts

--- prior_localization/run_scripts/01_stage_data.py
+++ prior_localization/run_scripts/01_stage_data.py
@@ -6,7 +6,6 @@ from prior_localization.fit_data import fit_session_ephys
 
 # User settings, ADAPT THESE TO YOUR CLUSTER
 input_dir = Path.home().joinpath('prior', 'inputs')
-output_dir = Path.home().joinpath('prior', 'outputs', 'ephys')
 
 # Here we use and online ONE instance because we need to actually access the database
 # We set it up to download to our chosen input_dir
@@ -22,4 +21,6 @@ for i, session_id in enumerate(bwm_df.eid.unique()):
     subject = bwm_df[bwm_df.eid == session_id].subject.unique()[0]
     # We are merging probes per session, therefore using a list of all probe names of a session as input
     probe_name = list(bwm_df[bwm_df.eid == session_id].probe_name)
-    _ = fit_session_ephys(one, session_id, subject, probe_name, output_dir=output_dir, stage_only=True)
+    # Run the fit session function with stage only flag to download all needed data, but not perform the decoding
+    # No data will be output, but since an output_dir is required, we just use the input dir as a front
+    _ = fit_session_ephys(one, session_id, subject, probe_name, output_dir=input_dir, stage_only=True)
--- prior_localization/run_scripts/run_ephys_decoding.py
+++ prior_localization/run_scripts/run_ephys_decoding.py
@@ -7,15 +7,17 @@ from prior_localization.fit_data import fit_session_ephys
 
 # Parse input arguments
 parser = argparse.ArgumentParser(description='Run decoding')
+parser.add_argument('output_dir')
 parser.add_argument('session_idx')
 parser.add_argument('pseudo_ids')
 args = parser.parse_args()
+output_dir = str(args.output_dir)
 session_idx = int(args.session_idx)
 pseudo_ids = list(args.pseudo_id)
 
 # Create an offline ONE instance, we don't want to hit the database when running so many jobs in parallel and have
 # downloaded the data before
-one = ONE(mode='local')
+one = ONE(base_url='https://openalyx.internationalbrainlab.org', mode='local')
 
 # Get info for respective eid from bwm_dataframe
 bwm_df = bwm_query(one=one, freeze='2022_10_initial')
@@ -24,10 +26,9 @@ subject = bwm_df[bwm_df.eid == session_id].subject.unique()[0]
 # We are merging probes per session, therefore using a list of all probe names of a session as input
 probe_name = list(bwm_df[bwm_df.eid == session_id].probe_name)
 
-# Need to set some config settings. Or change the config file and save it with the outputs. What is better?
-# Maybe the latter because it keeps a record and doesn't require the endless names
+# Run the decoding for the current set of pseudo ids.
 results = fit_session_ephys(
-        one, session_id, subject, probe_name, model='optBay', pseudo_ids=pseudo_ids, target='pLeft',
-        align_event='stimOn_times', time_window=(-0.6, -0.1), output_dir=None, regions='single_regions',
-        min_trials=150, motor_residuals=False, stage_only=False, integration_test=False
+        one, session_id, subject, probe_name, output_dir=output_dir, pseudo_ids=pseudo_ids, target='pLeft',
+        align_event='stimOn_times', time_window=(-0.6, -0.1), model='optBay', n_runs=10, compute_neurometrics=False,
+        motor_residuals=False
 )
