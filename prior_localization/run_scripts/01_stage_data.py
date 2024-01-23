bb4071956f3b4f25f7d6940947b82463cd5c6cec
juhuntenburg <ju.huntenburg@gmail.com>
2024-01-23 17:07:18 +0100

    fix stage script

--- prior_localization/run_scripts/01_stage_data.py
+++ prior_localization/run_scripts/01_stage_data.py
@@ -14,7 +14,7 @@ ONE.setup(base_url='https://openalyx.internationalbrainlab.org', cache_dir=input
 one = ONE(base_url='https://openalyx.internationalbrainlab.org')
 
 # Get brainwidemap dataframe
-bwm_df = bwm_query(one=one, freeze='2022_10_initial')
+bwm_df = bwm_query(one=one, freeze='2023_12_bwm_release')
 
 # Loop over all sessions to stage ephys data
 for i, session_id in enumerate(bwm_df.eid.unique()):
@@ -22,6 +22,7 @@ for i, session_id in enumerate(bwm_df.eid.unique()):
     subject = bwm_df[bwm_df.eid == session_id].subject.unique()[0]
     # We are merging probes per session, therefore using a list of all probe names of a session as input
     probe_name = list(bwm_df[bwm_df.eid == session_id].probe_name)
+    probe_name = probe_name[0] if len(probe_name) == 1 else probe_name
     # Run the fit session function with stage only flag to download all needed data, but not perform the decoding
     # No data will be output, but since an output_dir is required, we just use the input dir as a front
     _ = fit_session_ephys(one, session_id, subject, probe_name, output_dir=input_dir, stage_only=True)
